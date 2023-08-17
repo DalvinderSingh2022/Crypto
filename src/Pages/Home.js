9f1d4f836128be08c182a7cb668db8f1ad4e4547
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2023-08-18 00:56:04 +0530

    Home Page completed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../components/useFetch';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
 
 const Home = () => {
+    const [coin, setCoin] = useState({});
+    const [isLoading, { isError, message }, list] = useFetch("/coins/markets?vs_currency=inr&per_page=10&page=0");
+
+    useEffect(() => {
+        setCoin({ ...list[0], rank: 1 })
+    }, [list]);
+
+    if (isError) return <Error message={message} />;
+    if (isLoading) return <Loading />;
     return (
-        <main className="home">
-            <img
-                src="https://media.istockphoto.com/id/1339415729/vector/3d-rendering-golden-bitcoins-on-blue-background.jpg?s=612x612&w=0&k=20&c=2xyWJa8FMra6Bw_XHCVJDBtgIqjqjVrxzdxAFIF1PSI="
-                alt="backgorund"
-                className='background'
-            />
-            <img
-                src="https://react-crypto-app-pi.vercel.app/static/media/btc.04101b0b163c035fa57e.png"
-                alt="coin"
-                className='coin'
-            />
-        </main>
+        <section className="home">
+            <div className="image">
+                <h1>Rank : {coin?.rank}</h1>
+                <span className="name">{coin?.name}</span>
+                <img src={coin?.image} alt="coin" className='coin' />
+            </div>
+            <div className="table">
+                <h1>Current Top Coins</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Coin</th>
+                            <th>Price</th>
+                            <th>24H Change</th>
+                            <th>Market Cap</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list.map((coin, index) => {
+                            return (
+                                <tr key={coin.id} onClick={() => setCoin({ ...coin, rank: (index + 1) })}>
+                                    <td>{index + 1}.</td>
+                                    <td>{coin.name}</td>
+                                    <td>₹ {coin.current_price}</td>
+                                    <td>{coin.market_cap_change_percentage_24h} %</td>
+                                    <td>₹ {coin.market_cap}</td>
+                                </tr>);
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </section>
     )
 }
 
